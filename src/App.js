227a823ae1e7475f003d87a4d3d1c347b5141306
zamirguliyev <zamirquliyev7@gmail.com>
2023-01-19 11:21:17 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Space } from "antd";
+import "./App.css";
+import AppFooter from "./Components/AppFooter";
+import AppHeader from "./Components/AppHeader";
+import PageContent from "./Components/PageContent";
+import SideMenu from "./Components/SideMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+      <Space className="SideMenuAndPageContent">
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+        </Space>
+      <AppFooter />
+    </div>
+  );
+}
+export default App;
